f0f117f6625e25dca5fb80243a8ca2ec55cc8689
Documenter.jl <documenter@juliadocs.github.io>
2024-12-05 12:04:00 +0000

    build based on ad8d970

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DopEFW","category":"page"},{"location":"#DopEFW","page":"Home","title":"DopEFW","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DopEFW.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DopEFW]","category":"page"}]
+}
